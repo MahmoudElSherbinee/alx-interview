01475db934eef9400f8ace6e333d0a435a8f085d
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-07-23 22:51:59 +0300

    try pascal 2

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,11 +11,12 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return[]
-    triangle = [[1]]
+    triangle = [[1]]    # Initialize the triangle with the first row
     for x in range(n - 1):
-        row = [1]
+        row = [1]   # Start each row with a '1'
         for y in range(x):
+            # Append the sum of the two elements above the current position
             row.append(triangle[-1][y] + triangle[-1][y + 1])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+        row.append(1)   # End each row with a '1'
+        triangle.append(row)    # Add the completed row to the triangle
+    return triangle     # Return the completed triangle
