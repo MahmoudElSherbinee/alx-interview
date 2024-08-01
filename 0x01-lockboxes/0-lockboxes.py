644be5a33f7e46bc9cd5590824a3129b5aeb0a51
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-08-01 11:54:11 +0300

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,18 +3,19 @@ def canUnlockAll(boxes):
     Determines if all boxes can be unlocked.
 
     Args:
-        boxes (list of lists of int): Each box is a list containing keys to other boxes.
+        boxes (list of lists of int):
+            Each box is a list containing keys to other boxes.
 
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
-    open_boxes = set([0])  # Use a set for better performance and to avoid duplicates
-    keys = set(boxes[0])  # Start with keys from the first box
-    
+    open_boxes = set([0])
+    keys = set(boxes[0])
+
     while keys:
         key = keys.pop()
         if key not in open_boxes:
             open_boxes.add(key)
             keys.update(boxes[key])
-    
+
     return len(open_boxes) == len(boxes)
